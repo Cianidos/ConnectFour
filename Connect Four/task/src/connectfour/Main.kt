74ed146048a5559825da36a6dd550796f9bc122c
Nikita Alexandrov <nikitosiks338@gmail.com>
2021-10-22 14:08:40 +0300

    firstly gameOne and gameSession separated

--- Connect Four/task/src/connectfour/Main.kt
+++ Connect Four/task/src/connectfour/Main.kt
@@ -22,39 +22,19 @@ sealed class Player {
     operator fun component1() = name
     operator fun component2() = score
 }
+typealias RawBoard = Map<Int, MutableList<Char>>
 
-class GameSession {
-    // TODO: seald Player1(name) Player2(name)
-    var currPlayer: Player = Player.One
-    var totalGames = 1
-    var currentGame = 1
-
-    // TODO: separate OneGame and GameSession
-    var height by Delegates.notNull<Int>()
-    var width by Delegates.notNull<Int>()
-
-    lateinit var board: Map<Int, MutableList<Char>>
-
-    fun initBoard() {
-        board = HashMap<Int, MutableList<Char>>().apply {
-            (1..width).forEach { this[it] = mutableListOf() }
-        }
+data class Board(val width: Int, val height: Int) {
+    var raw: RawBoard = HashMap<Int, MutableList<Char>>().apply {
+        (1..width).forEach { this[it] = mutableListOf() }
     }
 
-    fun start() {
-        InitState()
-    }
-
-    fun putSign(column: Int, sign: Char) {
-        board[column]?.add(sign)
-    }
-
-    fun printBoard() {
+    fun print() {
         val header = (1..width).joinToString(" ", " ", " ")
         val footer = List(width) { "═" }.joinToString("╩", "╚", "╝")
         val body = List(height) { h ->
             List(width) { w ->
-                board[w + 1]?.getOrElse(height - h - 1) { " " }
+                raw[w + 1]?.getOrElse(height - h - 1) { " " }
             }.joinToString("║", "║", "║")
         }.joinToString("\n")
 
@@ -63,7 +43,88 @@ class GameSession {
         println(footer)
     }
 
-    fun InitState() {
+    fun putSign(column: Int, sign: Char) {
+        raw[column]?.add(sign)
+    }
+}
+
+class OneGame(starterPlayer: Player, width: Int, height: Int) {
+    /* TODO: data class Player(score)
+        Player1, Player2
+    */
+
+    private var currPlayer = starterPlayer
+    private var board: Board = Board(width, height)
+
+    init {
+        board.print()
+        AskCurrPlayer()
+    }
+
+    var firstScore: Int = 0
+    var secondScore: Int = 0
+
+
+    fun AskCurrPlayer() {
+        println("${currPlayer.name}'s turn:")
+        CurrPlayerTurnGet()
+    }
+
+    fun CurrPlayerTurnGet() {
+        val line = readLine()!!.trim()
+        when (val result = Validation(board, line)) {
+            is Validation.Error -> {
+                println(result.text)
+                AskCurrPlayer()
+            }
+            is Validation.Ok -> {
+                val sig = when (currPlayer) {
+                    Player.One -> 'o'
+                    Player.Two -> '*'
+                }
+                board.putSign(result.column, sig)
+                board.print()
+                when (val r = CheckWin(board, result.column)) {
+                    is CheckWin.Draw -> {
+                        currPlayer++
+                        currPlayer = !currPlayer
+                        currPlayer++
+                        currPlayer = !currPlayer
+                        println("It is a draw")
+                        EndOfGame()
+                    }
+                    is CheckWin.Win -> {
+                        currPlayer++
+                        currPlayer++
+                        println(r.text(currPlayer.name))
+                        EndOfGame()
+                    }
+                    CheckWin.NotEnd
+                    -> {
+                        currPlayer = !currPlayer
+                        AskCurrPlayer()
+                    }
+                }
+            }
+            is Validation.End -> EndOfGame()
+        }
+    }
+
+    fun EndOfGame() {
+
+    }
+}
+
+class GameSession {
+    var currPlayer: Player = Player.One
+    var totalGames = 1
+    var currentGame = 1
+
+    var height by Delegates.notNull<Int>()
+    var width by Delegates.notNull<Int>()
+
+
+    fun start() {
         Greetings()
     }
 
@@ -147,8 +208,7 @@ class GameSession {
 
     fun Announcement() {
         println(
-            """${Player.One.name} VS ${Player.Two.name}
-               $height X $width board""".trimIndent()
+            "${Player.One.name} VS ${Player.Two.name}\n$height X $width board"
         )
         if (totalGames != 1) {
             println("Total $totalGames games")
@@ -157,60 +217,16 @@ class GameSession {
     }
 
     fun StartOfGame() {
-        initBoard()
         if (totalGames == 1) {
             println("Single game")
         } else {
             println("Game #$currentGame")
         }
-        printBoard()
-        AskCurrPlayer()
+        OneGame(currPlayer, width, height)
+        // TODO start game, take result
+        EndOfGame()
     }
 
-    fun AskCurrPlayer() {
-        println("${currPlayer.name}'s turn:")
-        CurrPlayerTurnGet()
-    }
-
-    fun CurrPlayerTurnGet() {
-        val line = readLine()!!.trim()
-        when (val result = Validation(this, line)) {
-            is Validation.Error -> {
-                println(result.text)
-                AskCurrPlayer()
-            }
-            is Validation.Ok -> {
-                val sig = when (currPlayer) {
-                    Player.One -> 'o'
-                    Player.Two -> '*'
-                }
-                putSign(result.column, sig)
-                printBoard()
-                when (val r = CheckWin(this, result.column)) {
-                    is CheckWin.Draw -> {
-                        currPlayer++
-                        currPlayer = !currPlayer
-                        currPlayer++
-                        currPlayer = !currPlayer
-                        println("It is a draw")
-                        EndOfGame()
-                    }
-                    is CheckWin.Win -> {
-                        currPlayer++
-                        currPlayer++
-                        println(r.text(currPlayer.name))
-                        EndOfGame()
-                    }
-                    CheckWin.NotEnd
-                    -> {
-                        currPlayer = !currPlayer
-                        AskCurrPlayer()
-                    }
-                }
-            }
-            is Validation.End -> EndOfGame()
-        }
-    }
 
     fun EndOfGame() {
         if (totalGames == 1)
@@ -238,20 +254,20 @@ class GameSession {
 
 sealed class CheckWin {
     companion object {
-        operator fun invoke(gameSession: GameSession, column: Int): CheckWin =
-            gameSession.board.run {
-                when {
-                    Point(
-                        column,
-                        gameSession.board[column]!!.lastIndex
-                    ).haveFour(gameSession)
-                    -> Win
-                    this.values.fold(true)
-                    { acc, list -> acc && list.size == gameSession.height }
-                    -> Draw("It is a draw")
-                    else -> NotEnd
-                }
+        operator fun invoke(board: Board, column: Int):
+                CheckWin = board.run {
+            when {
+                Point(
+                    column,
+                    raw[column]!!.lastIndex
+                ).haveFour(raw)
+                -> Win
+                this.raw.values.fold(true)
+                { acc, list -> acc && list.size == height }
+                -> Draw("It is a draw")
+                else -> NotEnd
             }
+        }
     }
 
     object Win : CheckWin() {
@@ -265,26 +281,24 @@ sealed class CheckWin {
 
 sealed class Validation {
     companion object {
-        operator fun invoke(gameSession: GameSession, suspect: String)
-                : Validation =
-            gameSession.run {
-                val res = suspect.toIntOrNull()
-                when {
-                    suspect == "end"
-                    -> End
+        operator fun invoke(board: Board, suspect: String): Validation {
+            val res = suspect.toIntOrNull()
+            return when {
+                suspect == "end"
+                -> End
 
-                    res == null
-                    -> Error("Incorrect column number")
+                res == null
+                -> Error("Incorrect column number")
 
-                    res !in 1..(width)
-                    -> Error("The column number is out of range (1 - $width)")
+                res !in 1..(board.width)
+                -> Error("The column number is out of range (1 - ${board.width})")
 
-                    board[res]?.size == height
-                    -> Error("Column $res is full")
+                board.raw[res]?.size == board.height
+                -> Error("Column $res is full")
 
-                    else -> Ok(res)
-                }
+                else -> Ok(res)
             }
+        }
     }
 
     data class Ok(val column: Int) : Validation()
@@ -293,24 +307,24 @@ sealed class Validation {
 }
 
 data class Point(val x: Int, val y: Int) {
-    fun getNear(gameSession: GameSession): List<Point> {
+    fun getNear(rawBoard: RawBoard): List<Point> {
         val l = List(3) { (x - 1)..(x + 1) }.flatten()
             .zip(((y - 1)..(y + 1)).flatMap { listOf(it, it, it) })
             .filter { it.first != x || it.second != y }
             .map { Point(it.first, it.second) }
-            .filter { it.isOnBoard(gameSession) }
+            .filter { it.isOnBoard(rawBoard) }
             .toList()
         return l
     }
 
-    fun isOnBoard(gameSession: GameSession) =
-        gameSession.board[x]?.getOrNull(y) != null
+    fun isOnBoard(rawBoard: RawBoard) =
+        rawBoard[x]?.getOrNull(y) != null
 
     operator fun minus(other: Point) = Point(x - other.x, y - other.y)
     operator fun plus(other: Point) = Point(x + other.x, y + other.y)
 
-    fun getListOfFours(gameSession: GameSession): List<List<Point>> {
-        val l = getNear(gameSession).map {
+    fun getListOfFours(rawBoard: RawBoard): List<List<Point>> {
+        val l = getNear(rawBoard).map {
             val vector = it - this
             val f = it + vector
             val s = f + vector
@@ -319,10 +333,10 @@ data class Point(val x: Int, val y: Int) {
         return l
     }
 
-    fun haveFour(gameSession: GameSession): Boolean =
-        !getListOfFours(gameSession)
-            .filter { it.filter { it2 -> it2.isOnBoard(gameSession) }.size == 4 }
-            .map { it.map { (x1, y1) -> gameSession.board[x1]?.get(y1) } }
+    fun haveFour(rawBoard: RawBoard): Boolean =
+        !getListOfFours(rawBoard)
+            .filter { it.filter { it2 -> it2.isOnBoard(rawBoard) }.size == 4 }
+            .map { it.map { (x1, y1) -> rawBoard[x1]?.get(y1) } }
             .none { it.reduce { acc, c -> if (acc == c) acc else null } != null }
 }
 
